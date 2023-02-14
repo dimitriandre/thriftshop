d4359d5c1c5b8ca23e5f9c8ddd50d549db11cba9
dimitriandre <dimitri@atmosclear.com>
2023-02-14 09:56:20 +0100

    setup db connection

---
+++ ThriftshopWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ThriftshopWeb.Models;
+
+namespace ThriftshopWeb.Data
+{
+    public class ApplicationDbContext :DbContext
+    {
+        public ApplicationDbContext(DbContext<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+    }
+}
