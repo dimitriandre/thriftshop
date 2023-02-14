505aaf9d1ab4321fb7b360924ab49a43c74c866f
dimitriandre <dimitri@atmosclear.com>
2023-02-14 11:18:37 +0100

    category controller

---
+++ ThriftshopWeb/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ThriftshopWeb.Data;
+
+namespace ThriftshopWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+
+        }
+
+        public IActionResult Index()
+        {
+            var objCategoryList = _db.Categories.ToList();
+            return View();
+        }
+    }
+}
