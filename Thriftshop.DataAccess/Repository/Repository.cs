7479c1a4aa05b9b9c2295923b3ec999f69361d51
dimitriandre <dimitri@atmosclear.com>
2023-02-15 14:42:31 +0100

    adding repository class

--- Thriftshop.DataAccess/Repository/Repository.cs
+++ Thriftshop.DataAccess/Repository/Repository.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Thriftshop.DataAccess.Repository.IRepository;
 using ThriftshopWeb.DataAccess;
 
@@ -12,35 +13,42 @@ namespace Thriftshop.DataAccess.Repository
     internal class Repository<T> : IRepository<T> where T : class
     {
         private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
 
         public Repository(ApplicationDbContext db)
         {
             _db = db;
+            this.dbSet = _db.Set<T>();
         }
 
         public void Add(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Add(entity);
         }
 
         public IEnumerable<T> GetAll()
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+            return query.ToList();
         }
 
         public T GetFirstOrDefault(Expression<Func<T, bool>> filter)
         {
-            throw new NotImplementedException();
+            IQueryable<T> query = dbSet;
+
+            query = query.Where(filter);
+
+            return query.FirstOrDefault();
         }
 
         public void Remove(T entity)
         {
-            throw new NotImplementedException();
+            dbSet.Remove(entity);
         }
 
         public void RemoveRange(IEnumerable<T> entity)
         {
-            throw new NotImplementedException();
+            dbSet.RemoveRange(entity);
         }
     }
 }
