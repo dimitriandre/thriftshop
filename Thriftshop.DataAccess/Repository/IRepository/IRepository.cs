51537af981152709ed3aeb57a86e822b9006b0e1
dimitriandre <dimitri@atmosclear.com>
2023-02-15 11:35:17 +0100

    IRepository Interface

---
+++ Thriftshop.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Thriftshop.DataAccess.Repository.IRepository
+{
+    internal interface IRepository<T> where T : class
+    {
+        //T - Category
+        IEnumerable<T> GetAll();
+    }
+}
