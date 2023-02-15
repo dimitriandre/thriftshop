385c0e4f103efad8b152ec260396897ac88969fd
dimitriandre <dimitri@atmosclear.com>
2023-02-15 14:54:49 +0100

    category repository

---
+++ Thriftshop.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Thriftshop.DataAccess.Repository.IRepository;
+using ThriftshopWeb.Models;
+
+namespace Thriftshop.DataAccess.Repository
+{
+    internal class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        public void Save()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Category obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
