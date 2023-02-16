569f444533476d5b2f60742a492b87c1adc9df4b
dimitriandre <dimitri@atmosclear.com>
2023-02-16 18:07:14 +0100

    products list page

---
+++ ThriftshopWeb/wwwroot/js/product.js
@@ -0,0 +1,21 @@
+﻿var dataTable;
+
+$(document).ready(function (){
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "gender", "width": "15%" }
+            //{ "data": "price", "width": "15%" },
+
+        ]
+
+    });
+}
