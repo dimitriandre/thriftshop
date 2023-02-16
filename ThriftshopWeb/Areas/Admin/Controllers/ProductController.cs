980303af627210ba4d5d1ed2665449a8258fc816
dimitriandre <dimitri@atmosclear.com>
2023-02-16 15:09:54 +0100

    update

--- ThriftshopWeb/Areas/Admin/Controllers/ProductController.cs
+++ ThriftshopWeb/Areas/Admin/Controllers/ProductController.cs
@@ -64,12 +64,12 @@ namespace ThriftshopWeb.Controllers
         //POST
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Upsert(Product obj)
+        public IActionResult Upsert(ProductVM obj, IFormFile file)
         {
 
             if (ModelState.IsValid)
             {
-                _unitOfWork.Product.Update(obj);
+                //_unitOfWork.Product.Update(obj);
                 _unitOfWork.Save();
                 return RedirectToAction("Index");
             }
