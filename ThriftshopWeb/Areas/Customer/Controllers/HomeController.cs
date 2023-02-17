62794b40a3c1b6e9d1591f68705b45eef7b6810a
dimitriandre <dimitri@atmosclear.com>
2023-02-17 14:00:56 +0100

    home page*

--- ThriftshopWeb/Areas/Customer/Controllers/HomeController.cs
+++ ThriftshopWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
+using Thriftshop.DataAccess.Repository.IRepository;
+using Thriftshop.Models;
 using ThriftshopWeb.Models;
 
 namespace ThriftshopWeb.Controllers
@@ -7,15 +9,18 @@ namespace ThriftshopWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,ItemCondition");
+            return View(productList);
         }
 
         public IActionResult Privacy()
