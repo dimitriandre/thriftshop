af3fb9ee1322a34da64c5daef81323d57ebde63b
dimitriandre <dimitri@atmosclear.com>
2023-02-17 08:30:58 +0100

    table products

--- ThriftshopWeb/wwwroot/js/product.js
+++ ThriftshopWeb/wwwroot/js/product.js
@@ -12,8 +12,10 @@ function loadDataTable() {
         "columns": [
             { "data": "name", "width": "15%" },
             { "data": "price", "width": "15%" },
-            { "data": "gender", "width": "15%" }
-            //{ "data": "price", "width": "15%" },
+            { "data": "gender", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            { "data": "itemCondition.name", "width": "15%" }
+
 
         ]
 
