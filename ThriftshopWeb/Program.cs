6fea7a63f33ca0acc499ea76646e407b57026d7f
dimitriandre <dimitri@atmosclear.com>
2023-02-20 11:06:43 +0100

    identity

--- ThriftshopWeb/Program.cs
+++ ThriftshopWeb/Program.cs
@@ -2,7 +2,7 @@ using Microsoft.EntityFrameworkCore;
 using Thriftshop.DataAccess.Repository.IRepository;
 using Thriftshop.DataAccess.Repository;
 using Thriftshop.DataAccess;
-
+using Microsoft.AspNetCore.Identity;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +11,8 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
     ));
+builder.Services.AddDefaultIdentity<IdentityUser>()
+	.AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 var app = builder.Build();
 
@@ -26,6 +28,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+app.UseAuthentication();
 
 app.UseAuthorization();
 
