b1404737ba54947fa14806507d895f0892239922
dimitriandre <dimitri@atmosclear.com>
2023-02-23 14:57:53 +0100

    filter order index page

--- ThriftshopWeb/Areas/Admin/Controllers/OrderController.cs
+++ ThriftshopWeb/Areas/Admin/Controllers/OrderController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 using Thriftshop.DataAccess.Repository.IRepository;
 using Thriftshop.Models;
+using Thriftshop.Utility;
 
 namespace ThriftshopWeb.Areas.Admin.Controllers
 {
@@ -19,11 +21,31 @@ namespace ThriftshopWeb.Areas.Admin.Controllers
 
 		#region API CALLS
 		[HttpGet]
-		public IActionResult GetAll()
+		public IActionResult GetAll(string status)
 		{
 			IEnumerable<OrderHeader> orderHeaders;
-
 			orderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser");
+
+
+            switch (status)
+            {
+                case "pending":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.PaymentStatusDelayedPayment);
+                    break;
+				case "inprocess":
+                    orderHeaders = orderHeaders.Where(u => u.PaymentStatus == SD.StatusInProcess);
+                    break;
+                case "completed":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusShipped);
+                    break;
+                case "approved":
+                    orderHeaders = orderHeaders.Where(u => u.OrderStatus == SD.StatusApproved);
+                    break;
+                default:
+                    break;
+
+            }
+
 			return Json(new { data = orderHeaders });
 		}
 		#endregion
